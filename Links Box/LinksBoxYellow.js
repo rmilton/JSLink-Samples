d564a4efcd703d6cbaaacf99b4a1231da46589c0
Ryan Milton <ryan@ryanmilton.com>
2015-03-02 16:34:45 -0600

    added comments

--- Links Box/LinksBoxYellow.js
+++ Links Box/LinksBoxYellow.js
@@ -2,7 +2,7 @@
     var overrideCtx = {};
     overrideCtx.Templates = {};
 
-    //Tempate overrides
+    // Header override
     overrideCtx.Templates.Header = function (ctx) {
 
         var html = '<div class="linksBox-wrapper">';
@@ -16,19 +16,20 @@
 
     }
 
+	// Item override
     overrideCtx.Templates.Item = function (ctx) {
 
         return '<a class="linksBox-item" href="' + ctx.CurrentItem.URL + '">' + ctx.CurrentItem["URL.desc"] + '</a>';
 
     }
 
-
+	// Footer override
     overrideCtx.Templates.Footer = function (ctx) {
 
         return '</div></div>';
 
     }
 
-
+	// Register template override
     SPClientTemplates.TemplateManager.RegisterTemplateOverrides(overrideCtx);
 })();
